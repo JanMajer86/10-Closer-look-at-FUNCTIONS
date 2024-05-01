72694c95d975fb77277a56fe328065ec5ca7110b
JanMajer86 <majer.jan.1986@gmail.com>
2024-05-01 19:42:34 +0200

    callback functions

--- script.js
+++ script.js
@@ -65,3 +65,36 @@ newPassport(hochmajer);
 checkIn(flight, hochmajer);
 
 */
+
+////////////////////////////////////////////
+// FUNCTIONS ACCEPTING CALLBACK FUNCTIONS //
+////////////////////////////////////////////
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...other] = str.split(' ');
+  return [first.toUpperCase(), ...other].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👏');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
