529c8401bf7ae2ec9bf83724d9af0abb3305c314
JanMajer86 <majer.jan.1986@gmail.com>
2024-05-02 12:15:29 +0200

    bind method fin

--- script.js
+++ script.js
@@ -208,3 +208,28 @@ lufthansa.buyPlane = function () {
 document
   .querySelector('.buy')
   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+// nová specifická funkce odvozená od "addTax" funkce
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+/// challenge
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+// for RATE
+const addVAT2 = addTaxRate(0.23);
+// for VALUE
+console.log(addVAT2(100));
+console.log(addVAT2(23));
